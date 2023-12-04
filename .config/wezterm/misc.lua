01ca6ea1e98665477d925f00b8f9e1b882f799ad
Jacob Rodal <jrodal@meta.com>
2023-12-04 13:55:27 -0800

    update wezterm windows

--- .config/wezterm/misc.lua
+++ .config/wezterm/misc.lua
@@ -6,8 +6,10 @@ local misc = {}
 misc.exit_behavior = "Close"
 misc.audible_bell = "Disabled"
 
-if wezterm.hostname() == "jrodal-850" or wezterm.hostname() == "jrodal-257" then
+if wezterm.hostname() == "jrodal-850" then
    misc.default_prog = { "pwsh", "-l" }
+elseif wezterm.hostname() == "jrodal-257"  then
+  misc.default_prog = { "C:/msys64/msys2_shell.cmd", "-defterm", "-no-start", "-ucrt64", "-use-full-path" }
 else
    misc.default_prog = nil
 end
