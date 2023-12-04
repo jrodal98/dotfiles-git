90b2a5e3370561cf22cd2d8ea2da29f6ddcb7b59
Jacob Rodal <jrodal@meta.com>
2023-12-04 11:32:29 -0800

    add jrodal-257 to windows hostnames

--- .config/wezterm/misc.lua
+++ .config/wezterm/misc.lua
@@ -6,10 +6,9 @@ local misc = {}
 misc.exit_behavior = "Close"
 misc.audible_bell = "Disabled"
 
-if wezterm.hostname() == "jrodal-850" then
+if wezterm.hostname() == "jrodal-850" or wezterm.hostname() == "jrodal-257" then
    misc.default_prog = { "pwsh", "-l" }
 else
    misc.default_prog = nil
 end
-
 return misc
